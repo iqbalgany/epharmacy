b37de945b70d7d71f33276b150cc47580047851b
Muhammad Iqbal Al Afgany <rismagany1203@gmail.com>
2026-01-29 13:10:11 +0700

    Add signin page layout with email input field

--- lib/presentations/pages/signin_page.dart
+++ lib/presentations/pages/signin_page.dart
@@ -8,8 +8,47 @@ class SigninPage extends StatefulWidget {
 }
 
 class _SigninPageState extends State<SigninPage> {
+  final GlobalKey _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text('Signin'),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SizedBox(height: 32),
+                Center(child: Icon(Icons.person, size: 80)),
+                SizedBox(height: 32),
+                TextFormField(
+                  enableSuggestions: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: 'input your email',
+                    hintStyle: TextStyle(color: Colors.black),
+                    prefixIcon: Icon(
+                      Icons.email_outlined,
+                      color: Colors.black,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
